4533a40c36725150b48e3ab04b1fc80d5f618b40
wasabibob <twilmer@pm.me>
2025-01-26 22:45:23 -0700

    added updates for pytest

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-import pytest
+import flask
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -9,9 +10,5 @@ def index():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-    
-# Test the index route
-def test_index(client):
-    response = client.get('/')
-    assert response.status_code == 200
-    assert b'Hello, World!' in response.data
+
+#test_version()

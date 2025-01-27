78dbd59ec70a7844918a69a768b1db7e49557803
wasabibob <twilmer@pm.me>
2025-01-26 22:43:58 -0700

    added pytest tests

---
+++ tests.py
@@ -0,0 +1,19 @@
+import pytest
+import sys
+
+def test_example():
+    assert 1 + 1 == 2
+
+# Test the python runtime vesrion
+def test_version():
+    if sys.version_info >= (3, 10):
+        print("Using Python 3.10 or newer.")
+    else:
+        print("Python version is older than 3.10.")
+    assert sys.version_info == (3, 10)
+
+# Test the index route
+def test_index(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'Hello, World!' in response.data
